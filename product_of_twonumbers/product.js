89ab7cc03e7656e3e15093e90327ec5666eb265e
Mahakanth <mahakanth9@gmail.com>
2023-08-15 13:14:17 +0530

    product of two numbers and student onject

---
+++ product_of_twonumbers/product.js
@@ -0,0 +1,15 @@
+
+
+
+const product =(a,b) =>{
+    return a*b;
+}
+
+console.log(product(3,4));
+
+const student= {
+    'name':'lakki',
+    'age':22,
+    'course':'backend'
+}
+console.log(student);
