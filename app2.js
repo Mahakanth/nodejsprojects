b0e605b71611141265e79aaeea696f07b5212587
Mahakanth <mahakanth9@gmail.com>
2023-08-18 22:32:14 +0530

    cleaning the code using module.exports

---
+++ app2.js
@@ -0,0 +1,67 @@
+const http= require ('http');
+
+const fs= require('fs');
+const { buffer } = require('stream/consumers');
+
+const server =http.createServer((req,res) =>{
+
+    //console.log(req.url ,req.method ,req.headers);
+
+    const url =req.url;
+    const method =req.method;
+
+    const body =[];
+
+    if(url==='/'){
+        res.write('<html>');
+        res.write('<head><title> Enter Message </title></head>');
+        res.write('<body>')
+        res.write('<h2> Message</h2>')
+
+        //const messages = fs.readFileSync('message.txt', 'utf-8').split('\n').filter(message => message.trim() !== '');
+        //for (const message of messages) {
+           // res.write(`<p>${message}</p>`);
+       // }
+        res.write('<form action="/message" method="POST"> <input type="text" name="message"><button type="submit" >SEND</button></form>');
+        res.write('</body>')
+        res.write('</html>')
+        return res.end();
+    }
+
+    if(url==="/message" && method ==="POST"){
+
+        req.on('data',(chunk) =>{
+
+            //console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end',() =>{
+            const parsedbody=Buffer.concat(body).toString();
+
+            //console.log(parsedbody);
+
+            const message=parsedbody.split("=")[1];
+            fs.writeFile('message.txt', message,err =>{
+                res.statusCode=302;
+                res.setHeader('Location','/')
+                return res.end();
+            });
+            
+        });
+
+
+    }
+
+    res.setHeader('content-type','html');
+    res.write('<html>');
+    res.write('<head><title> My first page </title></head>');
+
+    res.write('<body><h2> my first node.js program</h2></body>');
+
+    res.write('</html>')
+    res.end();
+
+
+});
+
+server.listen(3001);
---
+++ routes.js
@@ -0,0 +1,59 @@
+
+const fs = require('fs'); 
+
+
+const requestHandler= (req,res) =>{
+    
+    const url =req.url;
+    const method =req.method;
+    if(url==='/'){
+        res.write('<html>');
+        res.write('<head><title> Enter Message </title></head>');
+        res.write('<body>')
+        res.write('<h2> Message</h2>')
+
+        const messages = fs.readFileSync('message.txt', 'utf-8').split('\n').filter(message => message.trim() !== '');
+        for (const message of messages) {
+            res.write(`<p>${message}</p>`);
+        }
+        res.write('<form action="/message" method="POST"> <input type="text" name="message"><button type="submit" >SEND</button></form>');
+        res.write('</body>')
+        res.write('</html>')
+        return res.end();
+    }
+
+    if(url==="/message" && method ==="POST"){
+
+        const body=[];
+
+        req.on('data',(chunk) =>{
+
+        //console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end',() =>{
+            const parsedbody=Buffer.concat(body).toString();
+
+        //console.log(parsedbody);
+
+            const message=parsedbody.split("=")[1];
+            fs.writeFileSync('message.txt', message);
+        });
+
+        res.statusCode=302;
+        res.setHeader('Location','/')
+        return res.end();
+    }
+
+    res.setHeader('content-type','html');
+    res.write('<html>');
+    res.write('<head><title> My first page </title></head>');
+
+    res.write('<body><h2> my first node.js program</h2></body>');
+
+    res.write('</html>')
+    res.end();
+    }
+
+
+module.exports =requestHandler;
